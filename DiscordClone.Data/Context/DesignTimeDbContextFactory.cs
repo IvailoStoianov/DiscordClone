d5c012387eb836fb2c5991e76589e0ef094a61d5
Ivailo <stIvailo05@gmail.com>
2025-02-15 12:56:44 +0200

    Fixed the problem with db migrations

--- DiscordClone.Data/Context/DesignTimeDbContextFactory.cs
+++ DiscordClone.Data/Context/DesignTimeDbContextFactory.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
 
 namespace DiscordClone.Data.Context
 {
@@ -7,8 +8,13 @@ namespace DiscordClone.Data.Context
     {
         public ApplicationDbContext CreateDbContext(string[] args)
         {
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
             var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
-            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=DiscordClone;Trusted_Connection=True;MultipleActiveResultSets=true");
+            optionsBuilder.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
 
             return new ApplicationDbContext(optionsBuilder.Options);
         }
--- DiscordClone.Data/Migrations/20250215105535_Initial.Designer.cs
+++ DiscordClone.Data/Migrations/20250215105535_Initial.Designer.cs
@@ -12,8 +12,8 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DiscordClone.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    [Migration("20250214153154_InitialCreate")]
-    partial class InitialCreate
+    [Migration("20250215105535_Initial")]
+    partial class Initial
     {
         /// <inheritdoc />
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
